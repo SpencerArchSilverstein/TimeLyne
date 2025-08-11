9632eeb4f403de0385df390c9292269b912aec0e
MusicTastingClubNU <musictastingclub@gmail.com>
2025-08-10 19:11:42 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,7 +148,9 @@ const Timelyne: React.FC = () => {
   return (
     <>
       <h1 className="text-center mt-10 font-bold text-5xl">TIMELYNE</h1>
-
+      <h5 className="text-center mt-10 font-bold text-xl">
+        Babe, I love you so fucking much 😍😘🥰
+      </h5>
       {!user ? (
         <div className="flex justify-center mt-10">
           <button
